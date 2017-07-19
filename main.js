e630dae1870b2384a269da7a3c5150dc7d784582
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-07-18 20:30:21 -0400

    added styling

--- main.js
+++ main.js
@@ -1,78 +1,49 @@
-fetch("http://www.recipepuppy.com/api/?q=gluten");
-  .then(function(response) {
-    if {
 
-    } else {
-      
-    }
+// create search button
+let search = document.getElementById('button_search')
+.addEventListener('click', onSearch);
 
-)}
+// introduce search box and content
+function onSearch() {
+  let content = document.getElementById('content');
+  let search_text = document.getElementById('search_box').value;
 
+// grab data from the API
+  fetch(
+      `https://crossorigin.me/http://www.recipepuppy.com/api/?q=${search_text}`
+    )
 
+    // if successful put inside the HTML for each result
+    .then(function(response) {
+      if (response.status !== 200) {
+        console.log(response.status);
+        return;
+      }
+      content.innerHTML = ''
+      response.json().then(function(data) {
+        data.results.forEach(function(result) {
 
+          // if the result doesn't have an image put in placeholder
+          if (result.thumbnail === '') {
+            result.thumbnail = "http://via.placeholder.com/150x150";
+          }
 
+          // a recipe's div for each result
+          let template = `
+          <article class="recipe">
+            <a href="${result.href}">
+            <img src="${result.thumbnail}">
+            <span>${result.title}</span>
+            </a>
+          </article>
+          `;
+          content.innerHTML += template;
+        });
+      });
+    })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   .then(function(response) {
-//     if (response.status !== 200) {
-//       console.log(response.status);
-//       return;
-//     }
-//     response.json().then(function(data) {
-//       console.log("test", response.url);
-//     });
-//   })
-//   .catch(function(err) {
-//     console.log("Fetch Error", err);
-//   });
-//
-//   function fetchGET("http://www.recipepuppy.com/api/"
-// ) {
-//     fetch("http://www.recipepuppy.com/api/"
-// )
-//       .then(function(response) {
-//         if (
-//           response.headers.get("content-type").indexOf("application/json") !== -1
-//         ) {
-//           // checking response header
-//           //.json() parses the response.
-//           return response.json();
-//         } else {
-//           throw new TypeError(
-//             'Response from "' + url + '" has unexpected "content-type"'
-//           );
-//         }
-//       })
-//       .then(function(data) {
-//         console.log('JSON from "' + url + '" parsed successfully!');
-//         console.log(data);
-//       })
-//       .catch(function(error) {
-//         console.error(error.message);
-//       });
-//   }
-//
-//   fetchGET(www.recipepuppy.com/api/);
+    // log this in case the data can not be received
+    .catch(function(err) {
+      console.log("Fetch Error :-S", err);
+    });
+}
